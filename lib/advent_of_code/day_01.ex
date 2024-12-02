6d9b48138a2b96945006f1168f44a2b218f5359f
Nick Jugganaikloo <nickjgniklu@gmail.com>
2024-12-02 11:31:32 -0700

    fix: day part 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,5 +1,17 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(args) do
+    String.split(args, "\n")
+    |> Enum.reject(&(String.trim(&1) == ""))
+    |> Enum.map(fn line ->
+      String.split(line, "   ")
+    end)
+    |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
+    |> Enum.unzip()
+    |> then(fn {a, b} -> [Enum.sort(a), Enum.sort(b)] end)
+    |> Enum.zip()
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum()
+    |> dbg()
   end
 
   def part2(_args) do
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,9 +3,8 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = AdventOfCode.Input.get!(1)
     result = part1(input)
 
     assert result
@@ -13,7 +12,7 @@ defmodule AdventOfCode.Day01Test do
 
   @tag :skip
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(1)
     result = part2(input)
 
     assert result
