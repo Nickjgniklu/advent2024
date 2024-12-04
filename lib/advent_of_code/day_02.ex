75e1cc5aa6885386d473f03e2b4e0551cacd0a45
Nick Jugganaikloo <nick.jugganaikloo@pdq.com>
2024-12-04 16:59:57 -0700

    d2p1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,32 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn sr ->
+      String.split(sr, " ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.map(&is_valid/1)
+    |> Enum.sum()
+    |> dbg()
+  end
+
+  defp is_increase(report) do
+    Enum.chunk_every(report, 2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a < b && abs(a - b) < 4 end)
+  end
+
+  defp is_decrease(report) do
+    Enum.chunk_every(report, 2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a > b && abs(a - b) < 4 end)
+  end
+
+  defp is_valid(report) do
+    if is_increase(report) || is_decrease(report) do
+      1
+    else
+      0
+    end
   end
 
   def part2(_args) do
